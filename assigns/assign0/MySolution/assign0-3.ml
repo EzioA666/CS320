a89f0b7e3403c401b1c2aea13f0a06a4c15d4f7a
Yunzhe Yu <yuyz2021@bu.edu>
2023-09-12 18:26:51 -0400

    new change for hw

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,14 +1,21 @@
 let chr = Char.chr
 let ord = Char.code
 let str(c0) = String.make 1 c0
+;;
+(* ****** ****** *)
+
+let string_init = String.init
+let string_length = String.length
+let string_get(cs, i0) = String.get cs i0
+;;
 
 let int2str i0 =
-  let rec loop i acc = 
-    if i = 0 && acc = "" then "0" (* Special case when i0 is 0 *)
-    else if i = 0 then acc
-    else 
-      let digit = i mod 10 in
-      let next_char = chr ((ord '0') + digit) in
-      loop (i / 10) (str(next_char) ^ acc)
-  in
-  if i0 < 0 then "-" ^ loop (-i0) "" else loop i0 ""
+  if i0 = 0 then "0"
+  else
+      let rec loop n acc =
+          if n = 0 then acc
+          else loop (n / 10) ((chr (48 + (n mod 10))) :: acc)
+      in
+      let chars = loop i0 [] in
+      string_init (List.length chars) (fun idx -> List.nth chars idx)
+;;
--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -1,6 +1,5 @@
 let string_init = String.init
 let string_length = String.length
 let string_get(cs, i0) = String.get cs i0
-let stringrev cs =
-  let len = string_length cs in
-  string_init len (fun i -> string_get(cs, len - 1 - i))
+let stringrev cs = 
+  string_init (string_length cs) (fun i -> string_get(cs, (string_length cs) - 1 - i))
